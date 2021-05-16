4f665806a3b9073a4f4b29e12b26d53ade44f445
Mauricio <mauricioxzaparoli@gmail.com>
2021-05-16 19:22:23 -0300

    Finalizacao do desafio

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User doesn't exist");
+    }
+
+    if (!user.admin) {
+      throw new Error("User must be an administrator!");
+    }
+    return this.usersRepository.list();
   }
 }
 
